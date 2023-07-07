d9857dbc2232a679fde40ed1dc8ee4424f0d68ae
Moosavi.MirHassan <17009501+mir-moo@users.noreply.github.com>
2023-07-07 18:00:03 +0330

    Update user.component.ts

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -3,14 +3,16 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-user',
     templateUrl: './user.component.html',
+    styleUrls: ['./user.component.css']
     // template: `
     //     <p>Template from User component</p>
     // `,
     // styleUrls: ['./user.component.css'] // ['style-1.css', 'style-2.css', ...]
-    styles: [`
-        p{color: orange; padding: 5px; background-color: black;}
-    `]
+    // styles: [`
+    //     p{color: orange; padding: 5px; background-color: black;}
+    // `]
 })
 export class UserComponent {
-    title='UserComponent'
+    title = 'UserComponent'
+    description = '<app-user>'
 }
