316b7041cf44f7c77d8b6615195055a605196a09
Moosavi.MirHassan <17009501+mir-moo@users.noreply.github.com>
2023-07-07 16:37:34 +0330

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'App component';
+  title = '<AppComponent>'
+  tag_name = '<app-root>'
 }
