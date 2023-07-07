048f909d60760a1e20385dda67c70956d8495e92
Moosavi.MirHassan <17009501+mir-moo@users.noreply.github.com>
2023-07-07 16:13:45 +0330

    Update app.module.ts
    updated automatically with angular cli
    UserComponent added into declarations array

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule
