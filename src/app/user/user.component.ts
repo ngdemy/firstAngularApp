449259ed788f7249c4902271bc66c78d2a4fa092
Moosavi.MirHassan <17009501+mir-moo@users.noreply.github.com>
2023-07-07 16:01:00 +0330

    Create User component

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-user',
+    // templateUrl: './user.component.html',
+    template: `
+        <p>Template from User component</p>
+    `,
+    // styleUrls: ['./user.component.css'] // ['style-1.css', 'style-2.css', ...]
+    styles: [`
+        p{color: orange; padding: 5px; background-color: black; display:inline;}
+    `]
+})
+export class UserComponent {
+
+}
